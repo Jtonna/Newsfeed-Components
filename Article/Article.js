f4899e9c6b8d70b6287afddfc0dea8ca82390bbe
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-04-10 15:39:53 -0700

    fixed my button issue & css
    Finished the project. Classmates helped me identify and fix a bug in my code. I also fixed the index.less not importing files from the right path.

--- Article/Article.js
+++ Article/Article.js
@@ -6,18 +6,18 @@ class Article {
     this.domElement = domElement;
 
     // create a reference to the ".expandButton" class. 
-    this.expandButton = document.querySelector('.expandButton');
+    this.expandButton = domElement.querySelector('.expandButton');
 
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'Read More!';
 
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', (expandArticleMethodName) => this.expandArticle(expandArticleMethodName));
+    this.expandButton.addEventListener('click', (artEvent) => this.expandArticle(artEvent));
   }
 
-  expandArticle(expandArticleMethodName) {
+  expandArticle(artEvent) {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    console.log(expandArticleMethodName.target);
+    console.log(artEvent.target);
     this.domElement.classList.toggle('article-open')
   }
 }
